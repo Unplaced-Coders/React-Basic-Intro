0c8679394ff24c018a6ec5712dadb3ca36028704
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2022-06-07 15:32:01 +0530

    day1 and day2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+    return (
+       <div>
+           <h1>Hello</h1>
+           <p>I am abhishek</p>
+       </div>
+    )
 }
+// export function Hello1(){
+//     return (
+//        <div>
+//            <h1>Hello1</h1>
+//            <p>I am abhishek1</p>
+//        </div>
+//     )
+// }
+export default App
 
-export default App;
+
+
+//arrow function
+// const Hello=()=>{
+
+// }
+
+//  const xyz=(a,b)=>{
+
+// }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Hello from "./App"
+// import  {Hello,Hello1}   from "./App"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// functions
+// function hello(a,b){
+//     return a+b
+// }
+// hello(1,2)
+
+// components
+// JSX 
+
+ReactDOM.render(<Hello />, document.getElementById("root"))
+
+
+
+
+
